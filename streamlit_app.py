4bfed703234b73637fb8a9a311ee7ecc765fd7f4
moritzzoepffel <60434547+moritzzoepffel@users.noreply.github.com>
2024-10-08 17:00:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('🎈 OSW KI App')
+st.image("https://oswald-online.de/wp-content/uploads/2023/10/Oswald_Logo_rot_schwarz.png")
 
 st.write('Hello world!')
